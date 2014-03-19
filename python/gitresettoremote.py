033e9a03033c26b878678a38124b7b672f87bd4e
Fenhl <fenhl@fenhl.net>
2014-03-19 19:01:30 +0000

    Add git reset-to-remote subcommand

---
+++ python/gitresettoremote.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Reset current git repository's state to be exactly like the remote.
+
+Usage:
+  git-reset-to-remote [options]
+  git-reset-to-remote -h | --help
+  git-reset-to-remote --version
+
+Options:
+  -h, --help              Print this message and exit.
+  --branch=<branch_name>  The branch to reset to [Default: master].
+  --remote=<remote_name>  The remote to reset to [Default: origin].
+  --version               Print version info and exit.
+"""
+
+import sys
+
+from docopt import docopt
+import subprocess
+import syncbin
+
+__version__ = syncbin.__version__
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='git reset-to-remote from fenhl/syncbin ' + __version__)
+    subprocess.check_call(['git', 'fetch', arguments['--remote']])
+    sys.exit(subprocess.call(['git', 'reset', '--hard', arguments['--remote'] + '/' + arguments['--branch']]))
