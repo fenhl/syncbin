c2b9630319082f9b84390672a301a234aa162e10
Fenhl <fenhl@fenhl.net>
2014-04-11 20:57:59 +0000

    Improve diskspace.py Reads disk spaces with decimals, importable, more options (including --help and --version), docstring

--- python/diskspace.py
+++ python/diskspace.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
+"""Show available space on main disk
+
+Usage:
+  diskspace [options]
+  diskspace -h | --help
+  diskspace --version
+
+Options:
+  -h, --help  Print this message and exit.
+  --bytes     Print the raw number of bytes instead of a human-readable format.
+  --debug     If an error occurs, print the traceback instead of a generic error message.
+  --version   Print version info and exit.
+  --zsh       For using in the zsh right prompt, produce no output if more than 1 GB available.
+"""
+
 import sys
 
 import re
 import subprocess
+import syncbin
+
+__version__ = syncbin.__version__
 
 def format_space(num_bytes):
     if num_bytes >= 2 ** 50:
@@ -23,27 +41,36 @@ def parse_space(space_string):
     match = re.match('([0-9.]+)([PTGMKB])', space_string)
     if match:
         amount, unit = match.group(1, 2)
-        return {
+        return int({
             'P': 2 ** 50,
             'T': 2 ** 40,
             'G': 2 ** 30,
             'M': 2 ** 20,
             'K': 2 ** 10,
             'B': 1
-        }[unit] * int(amount)
+        }[unit] * float(amount))
     return int(space_string)
 
 ONE_GIG = 2 ** 30
 
-try:
-    output = subprocess.check_output(['df', '-hl', '/'], stderr=subprocess.STDOUT).decode('utf-8')
-    space = parse_space(output.splitlines()[1].split()[3])
-except:
-    print('[disk: error]')
-    sys.exit(1)
-
-if '--zsh' not in sys.argv or space < ONE_GIG:
-    if '--bytes' in sys.argv:
-        print(str(space))
-    else:
-        print('[disk: ' + format_space(space) + ']')
+if __name__ == '__main__':
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(__doc__)
+        sys.exit()
+    if '--version' in sys.argv:
+        print('diskspace from fenhl/syncbin ' + __version__)
+        sys.exit()
+    try:
+        output = subprocess.check_output(['df', '-hl', '/'], stderr=subprocess.STDOUT).decode('utf-8')
+        space = parse_space(output.splitlines()[1].split()[3])
+    except:
+        if '--debug' in sys.argv:
+            raise
+        else:
+            print('[disk: error]')
+            sys.exit(1)
+    if '--zsh' not in sys.argv or space < ONE_GIG:
+        if '--bytes' in sys.argv:
+            print(str(space))
+        else:
+            print('[disk: ' + format_space(space) + ']')
