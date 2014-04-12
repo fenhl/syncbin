601d461a5992838e3259b8d88cc56dada9877d04
Fenhl <fenhl@fenhl.net>
2014-04-12 20:02:36 +0000

    Add --no-internet-test option to syncbin script and improve formatting of syncbin-startup --help

--- python/syncbin.py
+++ python/syncbin.py
@@ -6,15 +6,16 @@ Usage:
   syncbin bootstrap [<setup>]
   syncbin hasinet
   syncbin install
-  syncbin startup [--ignore-lock]
+  syncbin startup [--ignore-lock] [--no-internet-test]
   syncbin update [<old> <new>]
   syncbin -h | --help
   syncbin --version
 
 Options:
-  -h, --help     Print this message and exit.
-  --ignore-lock  When used with the `startup' subcommand, ignore the lock that prevents 2 startup scripts from running in parallel.
-  --version      Print version info and exit.
+  -h, --help          Print this message and exit.
+  --ignore-lock       When used with the `startup' subcommand, release and ignore the lock that prevents the startup script from running multiple times at once.
+  --no-internet-test  When used with the `startup' subcommand, do not run `syncbin-hasinet' to test for internet connectivity, but run all other startup scripts regardless.
+  --version           Print version info and exit.
 """
 
 import sys
@@ -41,7 +42,7 @@ if __name__ == '__main__':
     elif arguments['install']:
         sys.exit(subprocess.call(['sh', os.path.join(os.environ.get('HUB', '/opt/hub'), 'fenhl', 'syncbin', 'config', 'install.sh')]))
     elif arguments['startup']:
-        sys.exit(subprocess.call(['syncbin-startup'] + (['--ignore-lock'] if arguments['--ignore-lock'] else [])))
+        sys.exit(subprocess.call(['syncbin-startup'] + (['--ignore-lock'] if arguments['--ignore-lock'] else []) + (['--no-internet-test'] if arguments['--no-internet-test'] else [])))
     elif arguments['update']:
         if arguments['<old>'] is None:
             sys.exit(subprocess.call(['syncbin-update']))
