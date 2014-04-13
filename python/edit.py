6a64b860c9374de7439e7d38ae418020fd163577
Fenhl <fenhl@fenhl.net>
2014-04-13 01:52:58 +0000

    Improvements to edit command Now lets you expliticly specify the editor to use using `--editor' (or `-c' as a shorthand for using Chocolat), and will follow symlinks when the pathlib module is available (such as on Python 3.4 or higher).

--- python/edit.py
+++ python/edit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
-"""
+"""Edit a file.
 
 Usage:
-  edit <command_or_file>
+  edit [options] <command_or_file>
   edit -h | --help
   edit --version
 
 Options:
-  -h, --help  Print this message and exit
-  --version   Print version info and exit
+  -c, --chocolat     Use Chocolat as the editor. Equivalent to `--editor=choc'.
+  -h, --help         Print this message and exit.
+  --editor=<editor>  The program with which to edit the file. Defaults to the value of the `VISUAL' or `EDITOR' environment variable, or `nano' if neither are set.
+  --version          Print version info and exit.
 """
 
 import sys
@@ -21,6 +23,16 @@ import syncbin
 
 __version__ = syncbin.__version__
 
+def path(command_or_file):
+    import pathlib
+    if command_or_file.startswith('~'):
+        path_string = os.path.expanduser(command_or_file)
+    elif '/' in command_or_file:
+        path_string = command_or_file
+    else:
+        path_string = subprocess.check_output(['which', command_or_file]).decode('utf-8').splitlines()[0]
+    return pathlib.Path(path_string).resolve()
+
 def resolve(command_or_file):
     if command_or_file.startswith('~'):
         return os.path.expanduser(command_or_file)
@@ -29,13 +41,24 @@ def resolve(command_or_file):
     else:
         return subprocess.check_output(['which', command_or_file]).decode('utf-8').splitlines()[0]
 
-def run(path):
-    return subprocess.call([os.environ.get('VISUAL', os.environ.get('EDITOR', 'nano')), path])
+def run(path, editor=None):
+    if editor is None:
+        editor = os.environ.get('VISUAL', os.environ.get('EDITOR', 'nano'))
+    return subprocess.call([editor, str(path)])
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='edit from fenhl/syncbin ' + __version__)
     try:
-        cmd_or_file = resolve(arguments['<command_or_file>'])
+        try:
+            cmd_or_file = path(arguments['<command_or_file>'])
+        except ImportError:
+            cmd_or_file = resolve(arguments['<command_or_file>'])
     except subprocess.CalledProcessError:
         sys.exit('[!!!!] edit: command ' + repr(arguments['<command_or_file>']) + ' not found')
-    sys.exit(run(cmd_or_file))
+    if arguments['--chocolat']:
+        editor = 'choc'
+    elif arguments['--editor']:
+        editor = arguments['--editor']
+    else:
+        editor = None
+    sys.exit(run(cmd_or_file, editor=editor))
