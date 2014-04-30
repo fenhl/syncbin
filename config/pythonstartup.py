2bb0e2e9c2df5c84c82c295d796dd59867aae947
Fenhl <fenhl@fenhl.net>
2014-04-30 03:20:35 +0000

    Add namespace function to Python startup file

--- config/pythonstartup.py
+++ config/pythonstartup.py
@@ -2,7 +2,15 @@
 
 import sys
 
+import code
+
 _syncbin_python_version_string = str(sys.version_info.major) + '.' + str(sys.version_info.minor)
 
 sys.ps1 = '[' + _syncbin_python_version_string + '>] '
 sys.ps2 = '[' + _syncbin_python_version_string + '…] '
+
+def namespace(initial=None):
+    if initial is None:
+        initial = {'__name__': '__console__', '__doc__': None}
+    code.InteractiveConsole(locals=initial).interact('')
+    return {k: v for k, v in initial.items() if not k.startswith('__'))
