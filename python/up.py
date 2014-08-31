05487b35fbe505f5acbf64d3e6d2baa98500564c
Fenhl <fenhl@fenhl.net>
2014-08-31 22:14:00 +0000

    Add up script

---
+++ python/up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.path.append('/opt/py')
+
+import blessings
+
+if __name__ == '__main__':
+    terminal = blessings.Terminal()
+    for _ in range(int(sys.argv[1]) if len(sys.argv) > 1 else 1):
+        print(terminal.move_up, end='', flush=True)
+
