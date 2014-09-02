e9bbc6d91a7f48c9e33f83e0f7f03592deccc9c9
Fenhl <fenhl@fenhl.net>
2014-09-02 22:05:11 +0000

    Python 3.2 compatibility

--- python/clear-eol.py
+++ python/clear-eol.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
+
+sys.path.append('/opt/py')
+
 import blessings
 
 if __name__ == '__main__':
     terminal = blessings.Terminal()
-    print(terminal.clear_eol, end='', flush=True)
-
+    print(terminal.clear_eol, end='')
+    sys.stdout.flush()
--- python/up.py
+++ python/up.py
@@ -9,5 +9,5 @@ import blessings
 if __name__ == '__main__':
     terminal = blessings.Terminal()
     for _ in range(int(sys.argv[1]) if len(sys.argv) > 1 else 1):
-        print(terminal.move_up, end='', flush=True)
-
+        print(terminal.move_up, end='')
+        sys.stdout.flush()
