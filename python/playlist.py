d8353488a20cb52c2e0f89ade32357f0dcd3db1e
Fenhl <fenhl@fenhl.net>
2014-10-03 22:45:58 +0000

    Add formatting and truncating to playlist

--- python/playlist.py
+++ python/playlist.py
@@ -22,6 +22,7 @@ from docopt import docopt
 import os
 import pathlib
 import random
+import re
 import subprocess
 import syncbin
 
@@ -47,4 +48,17 @@ if __name__ == '__main__':
     elif arguments['add-random']:
         sys.exit(subprocess.call(['mpc', 'add', random.choice(subprocess.check_output(['mpc', 'ls', arguments['<path>']]).decode('utf-8').strip().split('\n'))]))
     else:
-        sys.exit(subprocess.call(['mpc', 'playlist', "--format=%position% %file%"]))
+        mpc_playlist = subprocess.Popen(['mpc', 'playlist', '--format=%position% %file%'], stdout=subprocess.PIPE)
+        for byte_line in mpc_playlist.stdout:
+            line = byte_line.decode('utf-8')
+            match = re.match('([0-9]+) (.*)\n', line)
+            if match:
+                index = int(match.group(1))
+                if index > 9999:
+                    print('[ ** ]', 'playlist truncated')
+                    mpc_playlist.communicate()
+                    sys.exit(mpc_playlist.returncode)
+                print('[{: >4}] {}'.format(index, match.group(2)))
+            else:
+                print('[ !! ]', line)
+        sys.exit(mpc_playlist.wait())
