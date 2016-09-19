ef5020eaecad462bd01a73bb9e3f1f06f94c3c82
Fenhl <fenhl@fenhl.net>
2016-09-19 18:19:10 +0000

    sleeptill: Sleep until tomorrow if no date specified with earlier time

--- python/sleeptill.py
+++ python/sleeptill.py
@@ -14,10 +14,8 @@ Options:
   --version      Print version info and exit.
 """
 
-from datetime import date
-from datetime import datetime
-from docopt import docopt
-from datetime import time as dtime
+import datetime
+import docopt
 import syncbin
 import time
 
@@ -25,17 +23,17 @@ __version__ = syncbin.__version__
 
 def sleep_until(end_date):
     """Block until a specified datetime."""
-    if end_date <= datetime.utcnow():
+    if end_date <= datetime.datetime.utcnow():
         return
-    time.sleep((end_date - datetime.utcnow()).total_seconds())
+    time.sleep((end_date - datetime.datetime.utcnow()).total_seconds())
 
 def verbose_sleep_until(end_date, io=None):
     import fancyio
-    
+
     def _verbose_sleep_inner(inner_io):
         sleep_line = fancyio.SleepLine(inner_io, end=end_date)
         sleep_line.join()
-    
+
     if io is None:
         with fancyio.IO() as new_io:
             _verbose_sleep_inner(inner_io=new_io)
@@ -43,12 +41,14 @@ def verbose_sleep_until(end_date, io=None):
         _verbose_sleep_inner(inner_io=io)
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='sleeptill from fenhl/syncbin ' + __version__)
+    arguments = docopt.docopt(__doc__, version='sleeptill from fenhl/syncbin ' + __version__)
     if arguments['<date>']:
-        end_date = datetime.strptime(arguments['<date>'] + ' ' + arguments['<time>'], '%Y-%m-%d %H:%M:%S')
+        end_date = datetime.datetime.strptime(arguments['<date>'] + ' ' + arguments['<time>'], '%Y-%m-%d %H:%M:%S')
     else:
         hours, minutes, seconds = (int(time_unit) for time_unit in arguments['<time>'].split(':'))
-        end_date = datetime.combine(date.today(), dtime(hours, minutes, seconds))
+        end_date = datetime.datetime.combine(datetime.date.today(), datetime.time(hours, minutes, seconds))
+        if end_date < datetime.datetime.utcnow():
+            end_date = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days=1), datetime.time(hours, minutes, seconds))
     if arguments['--verbose']:
         verbose_sleep_until(end_date)
     else:
