f1365c8934973b26f7228351f1af08a2f3fb8011
Fenhl <fenhl@fenhl.net>
2016-11-22 04:14:38 +0000

    Fix a variable name in mail rprompt script

--- rprompt/mail.sh
+++ rprompt/mail.sh
@@ -2,7 +2,7 @@
 
 for mail_dir in $mailpath; do
     if [[ -d "$mail_dir" ]]; then
-        num_mail=$(($(ls -af "$mail_path/cur" | wc -l) - 2))
+        num_mail=$(($(ls -af "$mail_dir/cur" | wc -l) - 2))
         if [[ "$num_mail" -gt 0 ]]; then
             echo "[mail: ${num_mail}]"
         fi
