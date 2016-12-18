472bcffd1f4723b0bf61d8eb0d941316ea61dff5
Fenhl <fenhl@fenhl.net>
2016-12-18 08:37:06 +0000

    Revert accidental changes to the volume BitBar plugin

--- bitbar/volume.5s.py
+++ bitbar/volume.5s.py
@@ -2,16 +2,12 @@
 
 import subprocess
 
-MUTED = '' #TODO
-LOW = '' #TODO
-HIGH = '' #TODO
-
 muted = subprocess.check_output(['osascript', '-e', 'output muted of (get volume settings)']).decode('utf-8') == 'true\n'
 volume = int(subprocess.check_output(['osascript', '-e', 'output volume of (get volume settings)']).decode('utf-8'))
 
 if muted:
-    print('|templateImage={}'.format(MUTED))
+    print('muted')
 elif volume < 6:
-    print('|templateImage={}'.format(LOW))
+    print('volume low')
 elif volume > 13:
-    print('|templateImage={}'.format(HIGH))
+    print('volume high')
