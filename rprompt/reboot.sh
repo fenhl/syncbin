93b3db3b517d1685458cbffd71ebf011b0daf87b
Fenhl <fenhl@fenhl.net>
2017-03-01 15:17:20 +0000

    Add more error checks to reboot rprompt

--- rprompt/reboot.sh
+++ rprompt/reboot.sh
@@ -1,9 +1,19 @@
 #!/bin/zsh
 
 if which jq &> /dev/null; then
-    if [[ "$(cat /opt/dev/reboot.json | jq '.schedule')" != "null" ]]; then
-        echo "[reboot: $(cat /opt/dev/reboot.json | jq -r '.schedule')]"
+    if [[ -a /opt/dev/reboot.json ]]; then
+        if [[ -f /opt/dev/reboot.json ]]; then
+            if [[ -r /opt/dev/reboot.json ]]; then
+                if [[ "$(cat /opt/dev/reboot.json | jq '.schedule')" != "null" ]]; then
+                    echo "[reboot: $(cat /opt/dev/reboot.json | jq -r '.schedule')]"
+                fi
+            else
+                echo '[reboot: can'"'"'t read /opt/dev/reboot.json]'
+            fi
+        else
+            echo '[reboot: /opt/dev/reboot.json is not a regular file]'
+        fi
     fi
 else
-    echo '[reboot: unknown (jq not found)]'
+    echo '[reboot: jq not found]'
 fi
