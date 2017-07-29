db4c53b6a4fee9ca8f87cc5e116fcc608882e896
Fenhl <fenhl@fenhl.net>
2017-07-29 17:42:56 +0000

    rprompt: Only show temparature warning above 85°C

--- rprompt/temperature.sh
+++ rprompt/temperature.sh
@@ -2,7 +2,7 @@
 
 if where vcgencmd &> /dev/null; then
     temperature="$(vcgencmd measure_temp)"
-    if echo "$temperature" | python3 -c $'import re, sys; sys.exit(0 if float(re.search("=(.*)\'", sys.stdin.read()).group(1)) >= 80.0 else 1)'; then
+    if echo "$temperature" | python3 -c $'import re, sys; sys.exit(0 if float(re.search("=(.*)\'", sys.stdin.read()).group(1)) >= 85.0 else 1)'; then
         echo "$temperature" | python3 -c $'import re, sys; print("[temperature: {}]".format(re.search("=(.*)\'", sys.stdin.read()).group(1)))'
     fi
 fi
