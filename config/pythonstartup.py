72cd9ff1cf222f6862cc3166d87a3df7dd2a1f9e
Fenhl <fenhl@fenhl.net>
2017-10-10 16:22:30 +0000

    Add asyncio shorthand to PYTHONSTARTUP

--- config/pythonstartup.py
+++ config/pythonstartup.py
@@ -2,6 +2,7 @@
 
 import sys
 
+import asyncio
 import code
 
 _syncbin_python_version_string = str(sys.version_info.major) + '.' + str(sys.version_info.minor)
@@ -14,3 +15,6 @@ def namespace(initial=None):
         initial = {'__name__': '__console__', '__doc__': None}
     code.InteractiveConsole(locals=initial).interact('')
     return {k: v for k, v in initial.items() if not k.startswith('__')}
+
+def sync(coro):
+    return asyncio.get_event_loop().run_until_complete(coro)
