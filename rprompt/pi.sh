45209303be6029d93b6a5b7089a10b02e95721db
Fenhl <fenhl@fenhl.net>
2017-10-27 00:21:17 +0000

    Fix pi rprompt

--- rprompt/pi.sh
+++ rprompt/pi.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if where vcgencmd &> /dev/null; then
-    is_under_voltage=$(($(vcgencmd get_throttled | pcregrep -o '0x[0-9A-Fa-f]+') & 0x01))
+    is_under_voltage=$(($(vcgencmd get_throttled | grep -Po '0x[0-9A-Fa-f]+') & 0x01))
     if [[ $is_under_voltage == 1 ]]; then
         echo '[pi: under-voltage]'
     fi
