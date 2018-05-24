abf4fda823e78b82e7ee93463aee2505199f5980
Fenhl <fenhl@fenhl.net>
2018-05-24 20:20:30 +0000

    Hide needrestart rprompt on WSL

--- rprompt/needrestart.sh
+++ rprompt/needrestart.sh
@@ -4,7 +4,11 @@ if which needrestart &> /dev/null && sudo -n true &> /dev/null; then
     # https://github.com/liske/needrestart/issues/22#issuecomment-209585427
     case $(sudo needrestart -b 2> /dev/null | grep 'NEEDRESTART-KSTA' | awk '{print $2}') in
         0)
-            echo '[needrestart: unknown]'
+            if grep -qE '(Microsoft|WSL)' /proc/version &> /dev/null; then
+                : # running on WSL, no needrestart support yet, skip for now
+            else
+                echo '[needrestart: unknown]'
+            fi
             ;;
         1)
             : # kernel is up-to-date
