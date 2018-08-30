a6359b75be927c35206911930b6f35a49f1534d4
Fenhl <fenhl@fenhl.net>
2018-08-30 08:43:42 +0000

    bitbar: Allow configuring diskspace plugin to monitor multiple volumes

--- bitbar/diskspace.2m.py
+++ bitbar/diskspace.2m.py
@@ -4,14 +4,25 @@ import sys
 
 sys.path.append('/opt/git/github.com/fenhl/syncbin/master/python')
 
+import basedir
 import diskspace
 import shutil
 
-usage = shutil.disk_usage('/')
-total = diskspace.Bytes(usage.total)
-available = diskspace.Bytes(usage.free)
+volumes = basedir.config_dirs('fenhl/syncbin.json').json().get('diskspace', {}).get('volumes', ['/'])
+volumes_data = {
+    volume: {'usage': shutil.disk_usage('/')}
+    for volume in volumes
+}
 
-if available < 5 * diskspace.ONE_GIG or available / total < 0.05:
-    print('disk: {}% ({})'.format(int(100 * available / total), available))
+for volume, volume_data in volumes_data.items():
+    volume_data['total'] = diskspace.Bytes(volume_data['usage'].total)
+    volume_data['available'] = diskspace.Bytes(volume_data['usage'].free)
+
+if any(volume_data['available'] < 5 * diskspace.ONE_GIG or volume_data['available'] / volume_data['total'] < 0.05 for volume_data in volumes_data.values()):
+    print('low disk space') #TODO icon
+    print('---')
+    for volume in volumes:
+        volume_data = volumes_data[volume]
+        print('{}: {}% ({})'.format(volume, int(100 * volume_data['available'] / volume_data['total']), volume_data['available']))
     print('---')
     print('Open DaisyDisk|bash=/usr/bin/open param1=-a param2=DaisyDisk terminal=false')
